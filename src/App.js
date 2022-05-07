aa9cc7d299448814e2593e7516d2916e8efef2cd
poojalpithwa <poojalpithwa@gmail.com>
2022-05-07 22:33:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import {React, Component} from 'react';
 import './App.css';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  class App extends Component{
+    constructor(){
+    super();
+    this.state={
+    title: "Event Finder App",
+    eventDetail : []
+      }
+    }
 
-export default App;
+    handleAddEvent= (e) =>{
+      e.preventDefault();
+      let eventDetail=this.state.eventDetail;
+      let name=this.refs.txtName.value;  
+      let date=this.refs.txtDate.value;  
+      let location=this.refs.txtLocation.value;  
+      let desc=this.refs.txtDesc.value;  
+      let newEvent ={
+       
+        "name": name,
+        "date": date,
+        "location": location,
+        "description":desc
+      }
+        eventDetail.push(newEvent);
+       this.setState({
+         eventDetail: eventDetail
+        })
+        console.log(this.state.eventDetail)
+
+        this.refs.addEvent.reset();
+     }
+
+     handleDeleteEvent =(i) =>{
+       let eventDetail=this.state.eventDetail;
+       eventDetail.splice(i,1);
+       this.setState({
+         eventDetail: eventDetail
+       });
+
+     }
+     render(){
+       let eventDetail= this.state.eventDetail;
+       return(
+      <div>
+        <Header/>
+           <div className='container'>
+           
+           
+           <form ref="addEvent" className="addEvent mt-3">
+              <label>Event Name</label>
+              <input type="text" ref="txtName" placeholder="Event Name" className="formField" required/>
+              <label>Event Date</label>
+              <input type="text" ref="txtDate" placeholder="Event Date" className="formField" required />
+              <label>Event Location</label>
+              <input type="text" ref="txtLocation" placeholder="Event Location" className="formField "required/>
+              <label>Event Description</label>
+              <input type="text" ref="txtDesc" placeholder="Event Description" className="formField" required/>
+              <button onClick={e => this.handleAddEvent(e)} className="myButton">Add</button>
+         </form>
+
+        {/* <EventCard data={this.state.eventDetail}/> */}
+
+
+         <table className='event-table mt-3'>
+           <tr>
+             <th>Event Name</th>
+             <th>Event Date</th>
+             <th>Event Location</th>
+             <th>Event Description</th>
+            </tr>
+           { eventDetail.map((data, i) =>
+            <tr key={i}>
+              <td>{data.name}</td>
+              <td>{data.date}</td>
+              <td>{data.location}</td>
+              <td>{data.description}</td>
+              <td>
+                <button onClick={e => this.handleDeleteEvent(e)} className="btn btn-danger">Delete</button>
+              </td>
+            </tr> )
+
+     }
+       </table>
+         </div>
+      </div>
+       ) }
+  }
+
+  export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-dark bg-dark">
+                <a class="navbar-brand ml-2" href="#">
+                  <span style={{marginLeft:'32px'}}>
+                  Event Finder
+                  </span>
+                </a>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
